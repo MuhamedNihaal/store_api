7c61006f62e8bec709762b6286b8ba68c73901d4
MuhamedNihaal <mnihalap@gmail.com>
2024-07-01 15:39:22 +0530

    Initial Commit

---
+++ starter/models/product.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product Name Must Be Provided']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Product Price Must Be Provided']
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    company: {
+        type: String,
+        enum: {
+            values: ['ikea', 'liddy', 'caressa', 'marcos'],
+            message: '{VALUE} is not Supported'
+        },
+        // enum: ['ikea', 'liddy', 'caressa', 'marcos']
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema)
